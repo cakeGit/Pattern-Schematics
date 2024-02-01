7acadea73a4ec339a239de4e061872dda1d9a8a3
cakeGit <65340665+cakeGit@users.noreply.github.com>
2024-02-01 23:30:48 +0000

    Fixed type / started functionality

--- src/main/java/com/cak/pattern_schematics/foundation/CloneTool.java
+++ src/main/java/com/cak/pattern_schematics/foundation/CloneTool.java
@@ -1,41 +1,42 @@
 package com.cak.pattern_schematics.foundation;
 
+import com.cak.pattern_schematics.foundation.mirror.PatternSchematicHandler;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.simibubi.create.content.schematics.client.SchematicTransformation;
 import com.simibubi.create.content.schematics.client.tools.ISchematicTool;
+import com.simibubi.create.content.schematics.client.tools.SchematicToolBase;
 import com.simibubi.create.foundation.render.SuperRenderTypeBuffer;
+import com.simibubi.create.foundation.utility.VecHelper;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Vec3i;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.client.gui.overlay.ForgeGui;
 
-public class CloneTool implements ISchematicTool {
+public class CloneTool extends SchematicToolBase {
   
-  @Override
-  public void init() {
-  
-  }
-  @Override
-  public void updateSelection() {
-  
-  }
   @Override
   public boolean handleRightClick() {
     return false;
   }
+  
   @Override
   public boolean handleMouseWheel(double delta) {
-    return false;
-  }
-  @Override
-  public void renderTool(PoseStack ms, SuperRenderTypeBuffer buffer, Vec3 camera) {
+    if (!schematicSelected || !selectedFace.getAxis().isHorizontal())
+      return true;
   
-  }
-  @Override
-  public void renderOverlay(ForgeGui gui, GuiGraphics graphics, float partialTicks, int width, int height) {
+    SchematicTransformation transformation = schematicHandler.getTransformation();
+    
+    if (!(schematicHandler instanceof PatternSchematicHandler patternSchematicHandler))
+      throw new RuntimeException("Clone tool bound in a normal SchematicHandler!");
   
-  }
-  @Override
-  public void renderOnSchematic(PoseStack ms, SuperRenderTypeBuffer buffer) {
+    patternSchematicHandler.setCloneData(
+        new Vec3i(-1, 0, -1),
+        new Vec3i(1, 0, 1)
+    );
+    schematicHandler.markDirty();
   
+    return true;
   }
   
 }
