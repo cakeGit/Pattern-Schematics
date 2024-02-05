9990fe932a2bb803b0aa2cf43fca848361aca093
cakeGit <65340665+cakeGit@users.noreply.github.com>
2024-02-05 22:02:09 +0000

    Fixed getprivatefield

--- src/main/java/com/cak/pattern_schematics/foundation/util/ReflectionUtils.java
+++ src/main/java/com/cak/pattern_schematics/foundation/util/ReflectionUtils.java
@@ -4,11 +4,14 @@ import java.lang.reflect.Field;
 
 public class ReflectionUtils {
   
+  @SuppressWarnings("unchecked cast")
   public static <T> T getPrivateField(Object instance, String name, Class<T> clazz) {
     try {
-      Field field = instance.getClass().getField(name);
+      Field field = instance.getClass().getDeclaredField(name);
       field.setAccessible(true);
-      return (T) field.get(field);
+      if (field.getType() != clazz)
+        throw new RuntimeException("Incorrect field type for getPrivateField");
+      return (T) field.get(instance);
     } catch (NoSuchFieldException | IllegalAccessException e) {
       throw new RuntimeException(e);
     }
