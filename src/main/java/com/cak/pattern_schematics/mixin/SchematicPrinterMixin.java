4efe6f8ccd177f91f6ff7f1d38a62b3fc0bfd42f
cakeGit <65340665+cakeGit@users.noreply.github.com>
2024-02-19 23:33:31 +0000

    Schematic loading handling for pattern schems

--- src/main/java/com/cak/pattern_schematics/mixin/SchematicPrinterMixin.java
+++ src/main/java/com/cak/pattern_schematics/mixin/SchematicPrinterMixin.java
@@ -2,10 +2,16 @@ package com.cak.pattern_schematics.mixin;
 
 import com.cak.pattern_schematics.content.PatternSchematicItem;
 import com.cak.pattern_schematics.foundation.mirror.PatternSchematicWorld;
+import com.cak.pattern_schematics.foundation.util.Vec3iUtils;
 import com.simibubi.create.content.schematics.SchematicPrinter;
 import com.simibubi.create.content.schematics.SchematicWorld;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Vec3i;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,6 +19,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = SchematicPrinter.class, remap = false)
@@ -44,5 +51,45 @@ public class SchematicPrinterMixin {
     }
   }
   
+//  @Redirect(method = "loadSchematic", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructureTemplate;getSize()Lnet/minecraft/core/Vec3i;"))
+//  private Vec3i getBounds(StructureTemplate instance) {
+//    //For pattern schematics template.getbounds != world.getbounds
+//    Vec3i size = lastThreadStructureTemplate.getSize();
+////
+////    if (blockReader instanceof  PatternSchematicWorld patternSchematicWorld)
+////      return Vec3iUtils.multiplyVec3i(
+////          size, new Vec3i(1, 1, 1)
+////              .subtract(patternSchematicWorld.cloneScaleMin)
+////              .offset(patternSchematicWorld.cloneScaleMax)
+////      );
+////    else
+//      return size;
+//  }
+  
+  @Redirect(method = "loadSchematic", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructureTemplate;placeInWorld(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructurePlaceSettings;Lnet/minecraft/util/RandomSource;I)Z"))
+  private boolean loadSchem(StructureTemplate instance, ServerLevelAccessor world,
+                            BlockPos blockPos1, BlockPos blockPos2, StructurePlaceSettings placeSettings,
+                            RandomSource randomSource, int i) {
+    if (world instanceof PatternSchematicWorld patternSchematicWorld) {
+      Vec3i minScale = patternSchematicWorld.cloneScaleMin;
+      Vec3i maxScale = patternSchematicWorld.cloneScaleMax;
+      
+      int k = 0;
+      for (int x = minScale.getX(); x <= maxScale.getX(); x++) {
+        for (int y = minScale.getY(); y <= maxScale.getY(); y++) {
+          for (int z = minScale.getZ(); z <= maxScale.getZ(); z++) {
+            BlockPos anchor = blockPos1.offset(
+                Vec3iUtils.multiplyVec3i(new Vec3i(x, y, z), patternSchematicWorld.sourceBounds.getLength().offset(1, 1, 1))
+            );
+            k++;
+            System.out.println("placed" + k + "" + Vec3iUtils.multiplyVec3i(new Vec3i(x, y, z), patternSchematicWorld.sourceBounds.getLength().offset(1, 1, 1)));
+            instance.placeInWorld(world, anchor, anchor, placeSettings, randomSource, i);
+          }
+        }
+      }
+      return true;
+    }
+    return instance.placeInWorld(world, blockPos1, blockPos2, placeSettings, randomSource, i);
+  }
   
 }
